a87a9c4647a8bc83ad4f69ed36dbd4af64bb542c
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-03-06 18:48:22 +0700

    update

---
+++ src/Requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "fc39dd420e97c55872b1e573d732afd8";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&laguage=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movies?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movies?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrerMovies: `/discover/movies?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMoviews: `/discover/movies?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movies?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcBqnloftIE-ELFOMnVjEJSYC9xKDyqrE",
+  authDomain: "netflix-clone-ff255.firebaseapp.com",
+  projectId: "netflix-clone-ff255",
+  storageBucket: "netflix-clone-ff255.appspot.com",
+  messagingSenderId: "622210570368",
+  appId: "1:622210570368:web:3d39cad7dafb1ac3a6ae65",
+};
